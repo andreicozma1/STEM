84cdb2dfd90541281673a6dec3a2b7ab7032ac50
Hunter Price <hunterprice4044@gmail.com>
2021-02-14 23:28:42 -0500

    started layout of help menu #27

--- src/main/java/HelpMenu.java
+++ src/main/java/HelpMenu.java
@@ -16,31 +16,38 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HelpMenu {
 	private Stage window;
 	private Scene menu;
+	private VBox layout;
 	private CloseButton closebutton;
 	
 	public HelpMenu(Scene prev){
 		window = new Stage();
 		window.setTitle("Help");
+		layout = new VBox(20);
 
 		closebutton = new CloseButton();
 		closebutton.setCloseButton(window);
-		
+
+		ScrollPane scrollPane = new ScrollPane();
 		Label label = new Label("Hello world");
-//		label.setStyle("-fx-text-fill: #839496");
-//
+		for (int i =0 ; i <30; i++) {
+			Label l = new Label("Hello world");
+			layout.getChildren().addAll(l);
+
+		}
+
 //		Button backButton = new Button("Back");
 //		backButton.setOnAction(e->window.setScene(prev));
 		
-		VBox layout = new VBox(20);
 		layout.getChildren().addAll(label, closebutton.getCloseButton());
-//		layout.setStyle("-fx-background-color: #002b36");
-		menu = new Scene(layout, 300, 300);
+		scrollPane.setContent(layout);
+		menu = new Scene(scrollPane, 300, 300);
 		window.setScene(menu);
 		window.show();
 
