f89a05a0b8ec5b1b05bf2734a5c119baa9166a31
Hunter Price <hunterprice4044@gmail.com>
2021-02-14 22:48:25 -0500

    made the help menu a popup window #27

--- src/main/java/HelpMenu.java
+++ src/main/java/HelpMenu.java
@@ -20,27 +20,29 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HelpMenu {
+	private Stage window;
 	private Scene menu;
 	private CloseButton closebutton;
 	
-	public HelpMenu(Stage window, Scene prev){
+	public HelpMenu(Scene prev){
+		window = new Stage();
+		window.setTitle("Help");
+
 		closebutton = new CloseButton();
 		closebutton.setCloseButton(window);
 		
 		Label label = new Label("Hello world");
 //		label.setStyle("-fx-text-fill: #839496");
-		
-		Button backButton = new Button("Back");
-		backButton.setOnAction(e->window.setScene(prev));
+//
+//		Button backButton = new Button("Back");
+//		backButton.setOnAction(e->window.setScene(prev));
 		
 		VBox layout = new VBox(20);
-		layout.getChildren().addAll(label, backButton, closebutton.getCloseButton());
+		layout.getChildren().addAll(label, closebutton.getCloseButton());
 //		layout.setStyle("-fx-background-color: #002b36");
 		menu = new Scene(layout, 300, 300);
-	}
-	
-	public void setMenu(Stage window){
-		window.setTitle("Help");
 		window.setScene(menu);
+		window.show();
+
 	}
 }
